bec7f95e70600d8a228f830b496d1f2a46976066
markianvoronovych <mark.voronovych@mail.utoronto.ca>
2022-10-24 17:16:48 -0400

    Added !

--- src/main/java/tutorial/HelloWorld.java
+++ src/main/java/tutorial/HelloWorld.java
@@ -16,7 +16,7 @@ public class HelloWorld {
             return "Hello";
         }
         if (decide % 5 == 0) {
-            return "World";
+            return "World!";
         }
         return String.valueOf(decide);
     }
