8610130d134c524e0f8bbbeca954da10cf0b49ca
Daniel Qiu <daniel.qiu@mail.utoronto.ca>
2022-11-08 00:49:46 -0500

    create initializer for createListingForm

--- src/main/java/forms/CreateListingForm.java
+++ src/main/java/forms/CreateListingForm.java
@@ -1,4 +1,40 @@
 package forms;
 
-public class CreateListingForm {
+public class CreateListingForm extends Form{
+
+
+    private float price;
+    private String description;
+    private String[] images;
+
+    public CreateListingForm(String title, float price, String description, String[] images) {
+        super(title);
+        this.price = price;
+        this.description = description;
+        this.images = images;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public String getImage(int imageIndex){
+        if (imageIndex >= images.length){
+            return "No Image";
+        }
+        else{
+            return images[imageIndex];
+        }
+    }
+
+    @Override
+    protected boolean validateForm() {
+        return false;
+    }
+
+    @Override
+    protected void submitForm() {
+    }
 }
