0800c46a17a6a5a7b0e321c2cb66afb0b8fe8ac9
markianvoronovych <mark.voronovych@mail.utoronto.ca>
2022-11-08 17:08:05 -0500

    Wrote all necessary getter and setters for Cart as well as the "getPrice" method. Rudimentary comments included but official comments are in progress.
    Cart page implementation started and minimal annotations and comments made.

--- src/main/java/pages/CartPage.java
+++ src/main/java/pages/CartPage.java
@@ -1,4 +1,37 @@
 package pages;
 
-public class CartPage {
+import features.Cart;
+import features.Listing;
+
+import java.util.ArrayList;
+
+public class CartPage extends Page{
+
+    //Hard to implement the logic here without the GUI and other people's logic being
+    //complete so consider this a rough draft until further notice.
+    private Cart cart;
+
+    public CartPage(String title, Cart itemCart){
+        super(title);
+        this.cart = itemCart;
+    }
+
+    //returns a list of the listings in the cart
+    public ArrayList<Listing> getCartItems(){
+        return this.cart.getItems();
+    }
+
+    //get the total price of all items in the cart
+    public int getCartPrice(){
+        return this.cart.getPrice();
+    }
+
+    //set the cart to a new cart
+    public void setCart(Cart newCart){
+        this.cart = newCart;
+    }
+
+    public Page goToCheckout(){
+        //NOT IMPLEMENTED, WARRANTS FURTHER DISCUSSION
+    }
 }
