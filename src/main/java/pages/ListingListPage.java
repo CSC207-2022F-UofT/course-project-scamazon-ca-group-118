efcf290dea059f6de3263b770d098127ae51a282
liu-samuel <liusamuelt@gmail.com>
2022-11-09 18:14:01 -0500

    Changed show listings to be dependent on database interactor

--- src/main/java/pages/ListingListPage.java
+++ src/main/java/pages/ListingListPage.java
@@ -1,10 +1,11 @@
 package pages;
 import features.Listing;
 import features.Search;
+import entities.DatabaseInteractor;
 
 import java.util.List;
 
-public class ListingListPage extends Page {
+public class ListingListPage extends Page implements DatabaseInteractor{
     private List<Listing> listings;
 
     // super call from page, assume every page title will be listings?
@@ -21,6 +22,27 @@ public class ListingListPage extends Page {
         this.listings = listings;
     }
 
+
+
+    // Implementing all methods from interface
+    // placeholder since I won't need this method
+    public void create() {
+        return;
+    }
+
+    public void update() {
+        return;
+    }
+    public void delete() {
+        return;
+    }
+
+    public T query() {
+        // TODO: Implement query which returns data, will have to access database which is not yet setup
+        return data;
+    }
+    T data = DatabaseInteractor.query();
+
     // will show listings
     // TODO: Will change once GUI is implemented
     public void showListings(List <Listing> listings) {
@@ -28,4 +50,6 @@ public class ListingListPage extends Page {
             System.out.println(List);
         }
     }
+
+    // I think the plan is to call showListings with our data variable as a parameter
 }
