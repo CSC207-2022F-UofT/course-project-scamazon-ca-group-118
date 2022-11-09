78f415c357756160320324b2f2d98b4afce3b410
ericg <eric.guo1230@gmail.com>
2022-11-09 15:22:50 -0500

    First changes

--- src/main/java/forms/RegisterForm.java
+++ src/main/java/forms/RegisterForm.java
@@ -1,4 +1,27 @@
 package forms;
 
-public class RegisterForm {
+public class RegisterForm extends Form{
+
+    private String username;
+    private String email;
+    private String password;
+    private String confirmpassword;
+
+    public RegisterForm(String title) {
+        super(title);
+    }
+
+    @Override
+    boolean validateForm() {
+        return false;
+    }
+
+    @Override
+    void submitForm() {
+
+    }
+    public void createUser(String username, String email, String password){
+        //call createUser method in user class using database interactor
+    }
+
 }
