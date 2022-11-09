d438ed2b34a15c30daaba6c8ad3e32acffc190cf
markianvoronovych <mark.voronovych@mail.utoronto.ca>
2022-11-09 15:46:53 -0500

    Fixed spacing issues

--- src/main/java/pages/CartPage.java
+++ src/main/java/pages/CartPage.java
@@ -5,48 +5,52 @@ import features.Listing;
 
 import java.util.ArrayList;
 
-public class CartPage extends Page{
+public class CartPage extends Page {
 
     private Cart cart;
 
-    /** Constructor for the Cart
+    /**
+     * Constructor for the Cart
      *
-     * @param title The title of the page.
+     * @param title    The title of the page.
      * @param itemCart A cart of Listing items.
      */
-    public CartPage(String title, Cart itemCart){
+    public CartPage(String title, Cart itemCart) {
         super(title);
         this.cart = itemCart;
     }
 
-    /** Get a list of all the items in the cart.
+    /**
+     * Get a list of all the items in the cart.
      *
      * @return An ArrayList of Listings in the cart.
      */
-    public ArrayList<Listing> getCartItems(){
+    public ArrayList<Listing> getCartItems() {
         return this.cart.getItems();
     }
 
-    /** Get the price of all the items in the cart.
+    /**
+     * Get the price of all the items in the cart.
      *
      * @return An int representing the total price of all items in cart.
      */
-    public int getCartPrice(){
+    public int getCartPrice() {
         return this.cart.getPrice();
     }
 
-    /** Set the cart to be a different instance of Cart.
+    /**
+     * Set the cart to be a different instance of Cart.
      *
      * @param newCart An instance of Cart.
      */
-    public void setCart(Cart newCart){
+    public void setCart(Cart newCart) {
         this.cart = newCart;
     }
 
     //Hard to implement the logic here without the GUI and other people's logic being
     //complete so consider this a rough draft until further notice.
-    public Page goToCheckout(){
+    public Page goToCheckout() {
         //NOT IMPLEMENTED, WARRANTS FURTHER DISCUSSION
-        //TODO Implement with the GUI after figuring out View
+        //TODO Implement with the GUI after figuring out View thing
     }
 }
