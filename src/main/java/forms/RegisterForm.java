6ccc579b4bbe969754bd700c3b5dcf01aeb2ea8e
ericg <eric.guo1230@gmail.com>
2022-11-09 15:59:30 -0500

    Revert "First changes"
    This reverts commit 78f415c357756160320324b2f2d98b4afce3b410.

--- src/main/java/forms/RegisterForm.java
+++ src/main/java/forms/RegisterForm.java
@@ -1,27 +1,4 @@
 package forms;
 
-public class RegisterForm extends Form{
-
-    private String username;
-    private String email;
-    private String password;
-    private String confirmpassword;
-
-    public RegisterForm(String title) {
-        super(title);
-    }
-
-    @Override
-    boolean validateForm() {
-        return false;
-    }
-
-    @Override
-    void submitForm() {
-
-    }
-    public void createUser(String username, String email, String password){
-        //call createUser method in user class using database interactor
-    }
-
+public class RegisterForm {
 }
--- src/main/java/pages/RegisterPage.java
+++ src/main/java/pages/RegisterPage.java
@@ -1,16 +1,4 @@
 package pages;
 
-import forms.RegisterForm;
-
-public class RegisterPage extends Page{
-
-    //Register form will be embedded inside
-    private RegisterForm form;
-
-    public RegisterPage(String title, RegisterForm form){
-        super(title);
-        this.form = form;
-    }
-
-    //submit page override
+public class RegisterPage {
 }
