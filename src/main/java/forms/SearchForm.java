86f2c81edeaa40cd96af599d7d23f98a55bd0787
markianvoronovych <mark.voronovych@mail.utoronto.ca>
2022-11-10 12:19:16 -0500

    Started searchform

--- src/main/java/forms/SearchForm.java
+++ src/main/java/forms/SearchForm.java
@@ -1,4 +1,21 @@
 package forms;
 
-public class SearchForm {
+public class SearchForm extends Form {
+    //TODO Make after database interactor is there
+
+    private String query;
+
+
+    @Override
+    boolean validateForm() {
+        return true;
+    }
+
+    @Override
+    void submitForm() {
+        if (this.validateForm()) {
+
+        }
+
+    }
 }
