01f3c60fc0c5170f9eafbd7b7ae26776ac59abc3
liu-samuel <liusamuelt@gmail.com>
2022-11-12 20:36:04 -0500

    removed lines I should not have added

--- src/main/java/database/ReviewDatabaseGateway.java
+++ src/main/java/database/ReviewDatabaseGateway.java
@@ -14,24 +14,4 @@ public interface ReviewDatabaseGateway {
      * @return User whose username is username, or an empty User
      */
     User getUserWithUsername(String username);
-
-    interface ListingDatabaseGateway {
-        /**
-         * Should retrieve the first x (10?) amount of listings to display on the page based on search keywords
-         * @param keyword which is what was entered into the search bar
-         * @return list of listings
-         */
-
-        List<Listing> getListingsWithSearch(String keyword);
-
-        /**
-         * Should retrieve the first x (10?) amount of listings to display on the page as a default
-         * @return list of listings
-         */
-
-        List<Listing> getListingsDefault();
-    }
-
-    class ListingDatabaseController {
-    }
 }
