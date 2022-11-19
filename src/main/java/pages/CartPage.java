04df1880584441f13ef472ddd702121d29528919
markianvoronovych <mark.voronovych@mail.utoronto.ca>
2022-11-18 20:01:21 -0500

    Finished the GUI completely, will only change when Navbar implementation becomes available.No comments yet, but coming soon.

--- src/main/java/pages/CartPage.java
+++ src/main/java/pages/CartPage.java
@@ -1,106 +1,86 @@
 package pages;
 
+import entities.User;
 import features.Cart;
 import features.Listing;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-public class CartPage extends Page {
+public class CartPage extends Page implements ActionListener {
 
-    private Cart cart;
+    final int WIDTH = 1280;
+    final int HEIGHT = 720;
+    private final JButton REMOVE;
+    private final JButton CHECKOUT;
+    private final JTable itemTable;
+    private JLabel priceTotal;
+    public Cart itemCart;
 
-    /**
-     * Constructor for the Cart
-     *
-     * @param title    The title of the page.
-     * @param itemCart A cart of Listing items.
-     */
-    public CartPage(String title, Cart itemCart) {
-        super(title);
-        //TODO Make title be "(User)'s Cart"
-        this.cart = itemCart;
+
+    public CartPage() {
+        super(User.getCurrentUser().getUsername() + "'s Cart");
+        this.setLayout(new GridLayout(1, 2));
+        this.itemCart = User.getCurrentUser().getCart();
+
+        this.setMinimumSize(new Dimension(WIDTH, HEIGHT));
+        this.setTitle("Scamazon.ca");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.itemTable = this.createItemTable();
+        JScrollPane scrollPane = new JScrollPane(this.itemTable);
+
+        //Create appropriate buttons and label
+        this.priceTotal = new JLabel("Total Price: $" + itemCart.getPrice());
+        this.REMOVE = new JButton("Remove Item");
+        this.REMOVE.addActionListener(this);
+        this.CHECKOUT = new JButton("Checkout");
+        this.CHECKOUT.addActionListener(this);
+
+        //Assign appropriate buttons to new button panel
+        JPanel buttonPanel = new JPanel(new GridLayout(3, 1, 10, 20));
+        buttonPanel.add(this.REMOVE);
+        buttonPanel.add(this.CHECKOUT);
+        buttonPanel.add(this.priceTotal);
+
+        //Assigning all panes and panels to frame
+        this.add(scrollPane);
+        this.add(buttonPanel);
     }
 
-    /**
-     * Get a list of all the items in the cart.
-     *
-     * @return An ArrayList of Listings in the cart.
-     */
-    public ArrayList<Listing> getCartItems() {
-        return this.cart.getItems();
-    }
-
-    /**
-     * Get the price of all the items in the cart.
-     *
-     * @return An int representing the total price of all items in cart.
-     */
-    public int getCartPrice() {
-        return this.cart.getPrice();
-    }
-
-    /**
-     * Set the cart to be a different instance of Cart.
-     *
-     * @param newCart An instance of Cart.
-     */
-    public void setCart(Cart newCart) {
-        this.cart = newCart;
-    }
-
-    public void render() {
-        JFrame frame = new JFrame(this.getTitle());
-        frame.setLayout(new FlowLayout());
-        frame.setSize(1280, 720);
-        JButton b = new JButton("Checkout");
-        JTable t = new JTable(cart.countItems(), 2);
-        t.setFont(new Font("Serif", Font.PLAIN, 20));
-        JScrollPane j = new JScrollPane(t);
-        j.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        t.getColumnModel().getColumn(0).setHeaderValue("Items");
-        t.getColumnModel().getColumn(1).setHeaderValue("Price");
-        ArrayList<Listing> items = this.getCartItems();
-        for (int i = 0; i < cart.countItems(); i++) {
-            t.setValueAt(items.get(i).getTitle(), i, 0);
-            t.setValueAt(items.get(i).getPrice(), i, 1);
+    public JTable createItemTable() {
+        ArrayList<Listing> items = this.itemCart.getItems();
+        String[][] data = new String[itemCart.countItems()][];
+        String[] list = new String[2];
+        for (int i = 0; i < this.itemCart.countItems(); i++) {
+            list = new String[]{items.get(i).getTitle(), String.valueOf(items.get(i).getPrice())};
+            data[i] = list;
         }
-        t.setBounds(190, 100, 900, cart.countItems() * 75);
-        frame.add(j);
-        frame.add(b);
-        frame.setVisible(true);
-
+        String[] columnNames = {"Item", "Price"};
+        DefaultTableModel model = new DefaultTableModel(data, columnNames);
+        JTable itemTable = new JTable(model);
+        itemTable.setFont(new Font("Serif", Font.PLAIN, 20));
+        return itemTable;
     }
 
-    //OVERLOADED
-    public void render(JFrame frame) {
-        frame.removeAll();
-        frame.setLayout(new FlowLayout());
-        frame.setSize(1280, 720);
-        JButton b = new JButton("Checkout");
-        JTable t = new JTable(cart.countItems(), 2);
-        t.setFont(new Font("Serif", Font.PLAIN, 20));
-        JScrollPane j = new JScrollPane(t);
-        j.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        t.getColumnModel().getColumn(0).setHeaderValue("Items");
-        t.getColumnModel().getColumn(1).setHeaderValue("Price");
-        ArrayList<Listing> items = this.getCartItems();
-        for (int i = 0; i < cart.countItems(); i++) {
-            t.setValueAt(items.get(i).getTitle(), i, 0);
-            t.setValueAt(items.get(i).getPrice(), i, 1);
-        }
-        t.setBounds(190, 100, 900, cart.countItems() * 75);
-        frame.add(j);
-        frame.add(b);
-        frame.setVisible(true);
-    }
-
-    //Hard to implement the logic here without the GUI and other people's logic being
-    //complete so consider this a rough draft until further notice.
     public Page goToCheckout() {
-        //NOT IMPLEMENTED, WARRANTS FURTHER DISCUSSION
-        //TODO Implement with the GUI after figuring out View thing
-        return new CheckoutPage("checkout");
+        return new CheckoutPage("Checkout");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == this.REMOVE) {
+            DefaultTableModel model = (DefaultTableModel) itemTable.getModel();
+            int row = this.itemTable.getSelectedRow();
+            itemCart.removeItem(row);
+            model.removeRow(row);
+            priceTotal.setText("Total Price: $" + itemCart.getPrice());
+        } else {
+            this.goToCheckout();
+        }
     }
 }
