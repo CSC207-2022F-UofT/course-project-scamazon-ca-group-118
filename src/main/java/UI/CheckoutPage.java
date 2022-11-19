b912a473137c39fbd7bb1c4ceb0f2be3d889631d
Helen <helenyu9243@gmail.com>
2022-11-19 18:11:20 -0500

    Created GUI for Checkout and fixed clean architecture dependencies

--- src/main/java/UI/CheckoutPage.java
+++ src/main/java/UI/CheckoutPage.java
@@ -1,8 +1,21 @@
 package UI;
 
-public class CheckoutPage extends Page {
+import forms.CheckoutForm;
 
-    public CheckoutPage(String title) {
-        super(title);
+import javax.swing.*;
+import java.awt.*;
+
+public class CheckoutPage extends Page {
+    private final JFrame FRAME;
+
+    public CheckoutPage() {
+        super("Checkout");
+        CheckoutForm form = new CheckoutForm();
+        this.FRAME = new JFrame();
+        this.FRAME.add(form);
+        this.FRAME.setSize(900, 600);
+        this.FRAME.setMinimumSize(new Dimension(450, 300));
+        this.FRAME.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.FRAME.setVisible(true);
     }
 }
--- src/main/java/forms/CheckoutForm.java
+++ src/main/java/forms/CheckoutForm.java
@@ -3,44 +3,95 @@ package forms;
 import useCase.checkout.CheckoutRequestModel;
 import useCase.checkout.CheckoutResponseModel;
 
-import java.time.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.LocalDate;
 
-public class CheckoutForm extends Form {
+public class CheckoutForm extends Form implements ActionListener {
     private final String USERNAME;
+    private final String NAME;
     private final String CARD_NUMBER;
     private final String CVV;
     private final LocalDate EXPIRATION; //Represents a date (year, month, day (yyyy-MM-dd))
     private final String ADDRESS;
     private CheckoutResponseModel responseModel;
+    private JLabel username;
+    private JLabel name;
+    private JLabel cardNumber;
+    private JLabel cvv;
+    private JLabel expiration;
+    private JLabel address;
+    private final JButton SUBMIT;
 
 
-
-    public CheckoutForm(String name, String number, String cvv, LocalDate expiration, String address) {
+    public CheckoutForm() {
         super("Checkout");
-        this.USERNAME = name;
-        this.CARD_NUMBER = number;
-        this.CVV = cvv;
-        this.EXPIRATION = expiration;
-        this.ADDRESS = address;
+
+        this.username = new JLabel("Username");
+        JTextField jtUsername = new JTextField();
+        this.name = new JLabel("Name");
+        JTextField jtName = new JTextField();
+        this.cardNumber = new JLabel("Card Number");
+        JTextField jtCardNumber = new JTextField();
+        this.cvv = new JLabel("CVV");
+        JTextField jtCVV = new JTextField();
+        this.expiration = new JLabel("Expiration Date");
+        JTextField jtExpiration = new JTextField();
+        this.address = new JLabel("Address");
+        JTextField jtAddress = new JTextField();
+        this.SUBMIT = new JButton("Submit");
+
+        JPanel jpForm = new JPanel();
+        jpForm.setLayout(new GridLayout(5, 2, 10, 5));
+        jpForm.add(this.username);
+        jpForm.add(this.name);
+        jpForm.add(this.cardNumber);
+        jpForm.add(this.cvv);
+        jpForm.add(this.expiration);
+        jpForm.add(this.address);
+        jpForm.add(this.SUBMIT);
+
+        JPanel jpMainPanel = new JPanel();
+        jpMainPanel.setLayout(new BorderLayout(10, 10));
+        jpMainPanel.add(jpForm, BorderLayout.NORTH);
+        jpMainPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        this.SUBMIT.addActionListener(this);
+
+        this.USERNAME = jtUsername.getText();
+        this.NAME = jtName.getText();
+        this.CARD_NUMBER = jtCardNumber.getText();
+        this.CVV = jtCVV.getText();
+        this.EXPIRATION = LocalDate.parse(jtExpiration.getText());
+        this.ADDRESS = jtAddress.getText();
     }
 
     @Override
     protected boolean validateForm() {
+        //current day
+        LocalDate today = LocalDate.now();
+
         //check number length is equal to 16
         if (!(this.CARD_NUMBER.length() == 16)) {
+            JOptionPane.showMessageDialog(this, "Please enter a valid card number.");
             return false;
-            //alert UI
         }
         //check cvv length is equal to 3
-        if (!(this.CVV.length() == 3)) {
+        else if (!(this.CVV.length() == 3)) {
+            JOptionPane.showMessageDialog(this, "Please enter a valid CVV.");
             return false;
-            //alert UI
         }
-        //check expiration is after today's date
-        LocalDate today = LocalDate.now();
-        if (!(this.EXPIRATION.isAfter(today))) {
+        //check expiration is after current date
+        else if (!(this.EXPIRATION.isAfter(today))) {
+            JOptionPane.showMessageDialog(this, "Card is expired.");
             return false;
-            //alert UI
+        }
+        //checks to make sure all fields are filled out
+        else if (this.USERNAME.isEmpty() || this.NAME.isEmpty() || this.ADDRESS.isEmpty()) {
+            JOptionPane.showMessageDialog(this, "All fields are required.");
+            return false;
         }
         return true;
     }
@@ -50,8 +101,15 @@ public class CheckoutForm extends Form {
         if (this.validateForm()) {
             CheckoutRequestModel requestModel = new CheckoutRequestModel(USERNAME);
             responseModel = new CheckoutResponseModel(requestModel);
-            //redirect to "home" page
-            //call function to remove items in cart from database
+            //redirects User back to ListingListPage
+            // return new ListingListPage();
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == this.SUBMIT) {
+            this.submitForm();
         }
     }
 }
