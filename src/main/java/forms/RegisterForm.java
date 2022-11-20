14a11b290715024d3ed180eee1736ab58766a20c
ericg <eric.guo1230@gmail.com>
2022-11-20 12:46:44 -0500

    Changes to GUI

--- src/main/java/forms/RegisterForm.java
+++ src/main/java/forms/RegisterForm.java
@@ -8,12 +8,11 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-class MyFrame
-        extends JFrame
+class MyForm
+        extends JPanel
         implements ActionListener {
 
     // Components of the Form
-    private Container c;
     private JLabel title;
     private JLabel name;
     private JTextField tname;
@@ -25,116 +24,99 @@ class MyFrame
     private JTextField confirmPass;
     private JButton sub;
     private JButton reset;
-    private JTextArea tout;
     private JLabel res;
-    private JTextArea resadd;
+    private JPanel form;
 
-    public MyFrame() {
-        setTitle("Registration Form");
-        setBounds(300, 90, 900, 600);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setResizable(false);
+    public MyForm() {
 
-        c = getContentPane();
-        c.setLayout(null);
+        this.form = new JPanel();
+        setBounds(300, 90, 600, 650);
+        form.setLayout(null);
+
 
         title = new JLabel("Registration Form");
         title.setFont(new Font("Arial", Font.PLAIN, 30));
         title.setSize(300, 30);
-        title.setLocation(300, 30);
-        c.add(title);
+        title.setLocation(175, 100);
+        form.add(title);
 
         name = new JLabel("Name");
         name.setFont(new Font("Arial", Font.PLAIN, 20));
         name.setSize(100, 20);
-        name.setLocation(75, 100);
-        c.add(name);
+        name.setLocation(100, 200);
+        form.add(name);
 
         tname = new JTextField();
         tname.setFont(new Font("Arial", Font.PLAIN, 15));
         tname.setSize(190, 20);
-        tname.setLocation(200, 100);
-        c.add(tname);
+        tname.setLocation(250, 200);
+        form.add(tname);
 
         mno = new JLabel("Email");
         mno.setFont(new Font("Arial", Font.PLAIN, 20));
         mno.setSize(100, 20);
-        mno.setLocation(75, 150);
-        c.add(mno);
+        mno.setLocation(100, 250);
+        form.add(mno);
 
         email = new JTextField();
         email.setFont(new Font("Arial", Font.PLAIN, 15));
-        email.setSize(150, 20);
-        email.setLocation(200, 150);
-        c.add(email);
+        email.setSize(190, 20);
+        email.setLocation(250, 250);
+        form.add(email);
 
 
         add = new JLabel("Password");
         add.setFont(new Font("Arial", Font.PLAIN, 20));
         add.setSize(100, 20);
-        add.setLocation(75, 200);
-        c.add(add);
+        add.setLocation(100, 300);
+        form.add(add);
 
         tadd = new JTextField();
         tadd.setFont(new Font("Arial", Font.PLAIN, 15));
-        tadd.setSize(150, 20);
-        tadd.setLocation(200, 200);
-        c.add(tadd);
+        tadd.setSize(190, 20);
+        tadd.setLocation(250, 300);
+        form.add(tadd);
 
         confirm = new JLabel("Confirm Password");
-        confirm.setFont(new Font("Arial", Font.PLAIN, 15));
-        confirm.setSize(125, 20);
-        confirm.setLocation(75, 250);
-        c.add(confirm);
+        confirm.setFont(new Font("Arial", Font.PLAIN, 17));
+        confirm.setSize(150, 20);
+        confirm.setLocation(100, 350);
+        form.add(confirm);
 
         confirmPass = new JTextField();
         confirmPass.setFont(new Font("Arial", Font.PLAIN, 15));
-        confirmPass.setSize(150, 20);
-        confirmPass.setLocation(200, 250);
-        c.add(confirmPass);
+        confirmPass.setSize(190, 20);
+        confirmPass.setLocation(250, 350);
+        form.add(confirmPass);
 
 
         sub = new JButton("Submit");
         sub.setFont(new Font("Arial", Font.PLAIN, 15));
         sub.setSize(100, 20);
-        sub.setLocation(150, 450);
+        sub.setLocation(100, 450);
         sub.addActionListener(this);
-        c.add(sub);
+        form.add(sub);
 
         reset = new JButton("Reset");
         reset.setFont(new Font("Arial", Font.PLAIN, 15));
         reset.setSize(100, 20);
-        reset.setLocation(270, 450);
+        reset.setLocation(300, 450);
         reset.addActionListener(this);
-        c.add(reset);
-
-        tout = new JTextArea();
-        tout.setFont(new Font("Arial", Font.PLAIN, 15));
-        tout.setSize(300, 400);
-        tout.setLocation(500, 100);
-        tout.setLineWrap(true);
-        tout.setEditable(false);
-        c.add(tout);
+        form.add(reset);
 
         res = new JLabel("");
         res.setFont(new Font("Arial", Font.PLAIN, 20));
         res.setSize(500, 25);
-        res.setLocation(100, 500);
-        c.add(res);
-
-        resadd = new JTextArea();
-        resadd.setFont(new Font("Arial", Font.PLAIN, 15));
-        resadd.setSize(200, 75);
-        resadd.setLocation(580, 175);
-        resadd.setLineWrap(true);
-        c.add(resadd);
+        res.setLocation(100, 400);
+        form.add(res);
 
         setVisible(true);
+
     }
 
-    // method actionPerformed()
-    // to get the action performed
-    // by the user and act accordingly
+    public JPanel getForm() {
+        return this.form;
+    }
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == sub) {
             String data
@@ -143,9 +125,7 @@ class MyFrame
                     + "Email : "
                     + email.getText() + "\n";
             String data1 = "Password : " + tadd.getText();
-            tout.setText(data + data1);
-            tout.setEditable(false);
-            res.setText("Registration Successfully..");
+            res.setText("Account Created!");
         } else if (e.getSource() == reset) {
             String def = "";
             tname.setText(def);
@@ -153,18 +133,10 @@ class MyFrame
             email.setText(def);
             confirmPass.setText(def);
             res.setText(def);
-            tout.setText(def);
-            resadd.setText(def);
         }
     }
 }
 
-class Registration {
-
-    public static void main(String[] args) throws Exception {
-        MyFrame f = new MyFrame();
-    }
-}
 
 public class RegisterForm extends Form{
 
