0e92c6e157a6eceea34477ba08e9dcb683409b45
liu-samuel <liusamuelt@gmail.com>
2022-11-21 17:24:12 -0500

    fixed errors in checkout

--- src/main/java/useCase/checkout/CheckoutResponseModel.java
+++ src/main/java/useCase/checkout/CheckoutResponseModel.java
@@ -1,9 +1,8 @@
 package useCase.checkout;
 
-<<<<<<< HEAD
+
 import java.io.IOException;
-=======
->>>>>>> origin/main
+
 
 public class CheckoutResponseModel {
     private String message;
@@ -14,13 +13,9 @@ public class CheckoutResponseModel {
      * @param requestModel the CheckoutRequestModel that will be used as an argument for the CheckoutInteractor
      *                     that this CheckoutResponseModel retrieves data from
      */
-<<<<<<< HEAD
+
     public CheckoutResponseModel(CheckoutRequestModel requestModel) throws IOException {
-        CheckoutInteractor interactor = new CheckoutInteractor(requestModel);
-=======
-    public CheckoutResponseModel(CheckoutRequestModel requestModel) {
         CheckoutInteractor interactor = new CheckoutInteractor(requestModel.getUsername());
         interactor.removeListings();
->>>>>>> origin/main
     }
 }
