aaa4f54b19ffdcfdc75fd3c32ad0b2196508950f
markianvoronovych <mark.voronovych@mail.utoronto.ca>
2022-11-27 17:16:37 -0500

    Put Comments for Search Database Gateway.

--- src/main/java/database/SearchDatabaseGateway.java
+++ src/main/java/database/SearchDatabaseGateway.java
@@ -6,5 +6,11 @@ import java.util.ArrayList;
 
 public interface SearchDatabaseGateway {
 
+    /**
+     * Uses a query to search the database for listings.
+     *
+     * @param query A String to be used to search for listings
+     * @return A ArrayList of Listings matyching query
+     */
     ArrayList<Listing> getListings(String query);
 }
