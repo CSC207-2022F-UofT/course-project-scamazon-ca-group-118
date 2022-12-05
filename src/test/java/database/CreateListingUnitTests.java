31dc883355c7a4e4ea22503db19e50b84e47ac60
liu-samuel <liusamuelt@gmail.com>
2022-12-05 02:24:13 -0500

    setup for createListingTests

---
+++ src/test/java/database/CreateListingUnitTests.java
@@ -0,0 +1,45 @@
+package database;
+
+import entities.Cart;
+import entities.User;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class CreateListingUnitTests {
+    public static final DatabaseController db = new DatabaseController();
+
+    @BeforeAll
+    public static void setUp() {
+        db.setListingTablePath("src/test/java/database/Listings.csv");
+        db.setUserTablePath("src/test/java/database/Users.csv");
+        File usersCSV = new File(db.getUserTablePath());
+        File listingsCSV = new File(db.getListingTablePath());
+    }
+
+    @BeforeEach
+    public void resetCSVFiles() throws IOException {
+        File usersCSV = new File(db.getUserTablePath());
+        if (usersCSV.delete()) {
+            usersCSV.createNewFile();
+        }
+        File listingsCSV = new File(db.getListingTablePath());
+        if (listingsCSV.delete()) {
+            listingsCSV.createNewFile();
+        }
+    }
+
+    @AfterAll
+    public static void deleteCSVFiles() {
+        File usersCSV = new File(db.getUserTablePath());
+        usersCSV.delete();
+        File listingsCSV = new File(db.getListingTablePath());
+        listingsCSV.delete();
+    }
+
+}
