e06dcbb239d9e45ae340e8ebd70885ad12a88013
liu-samuel <liusamuelt@gmail.com>
2022-12-08 14:51:26 -0500

    add comments to database directory

--- src/main/java/database/RegisterDatabaseGateway.java
+++ src/main/java/database/RegisterDatabaseGateway.java
@@ -5,6 +5,5 @@ import java.io.IOException;
 public interface RegisterDatabaseGateway {
     boolean checkUserWithUsername(String username) throws IOException;
     boolean checkUserWithEmail(String email) throws IOException;
-    // TODO: Checks database to see if email exists
     void createUser(String username, String email, String password);
 }
