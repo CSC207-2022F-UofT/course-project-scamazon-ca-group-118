4fb94e8f370ce53c656815ab66ab93fb817e0940
Eriq123 <eric.guo1230@gmail.com>
2022-12-08 15:26:35 -0500

    Fixing minor error and deleting unit tests

--- src/test/java/use_case/register/RegisterInteractorUnitTests.java
+++
@@ -1,4 +0,0 @@
-package use_case.register;
-
-public class RegisterInteractorUnitTests {
-}
--- src/test/java/use_case/register/RegisterResponseModelUnitTests.java
+++
@@ -1,8 +0,0 @@
-// package useCase.Register;
-//
-// import org.junit.jupiter.api.Test;
-//
-// public class RegisterResponseModelUnitTests {
-//     @Test
-//     void TestLoginWithNoUsername
-// }
